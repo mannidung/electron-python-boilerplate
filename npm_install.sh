43655139c420c603d088121a32215fdc6814c851
Pär Persson Mattsson <p.perssonmattsson@gmail.com>
2019-04-24 20:17:31 +0200

    Adds zerorpc to package.json; adds package-lock.json; corrects errors in and renames electron-rebuild.sh; updates readme and .gitignore

--- npm_install.sh
+++ npm_install.sh
@@ -2,8 +2,8 @@
 
 # Edit these values as necessary
 VIRTUAL_ENV_PATH="./venv"
-NODE_MODULES_PATH = "./node_modules"
-PYTHON2_EXEC_PATH = "/usr/bin/python2.7"
+NODE_MODULES_PATH="./node_modules"
+PYTHON2_EXEC_PATH="/usr/bin/python2.7"
 
 # Move the existing python symlink to be able to create a new link to Python2.7
 mv ${VIRTUAL_ENV_PATH}/bin/python ${VIRTUAL_ENV_PATH}/bin/python_temp
@@ -20,5 +20,4 @@ npm install
 rm ${VIRTUAL_ENV_PATH}/bin/python
 
 # Reset to original symlink
-#ln -s ${VIRTUAL_ENV_PATH}/bin/python3 ${VIRTUAL_ENV_PATH}/bin/python
 mv ${VIRTUAL_ENV_PATH}/bin/python_temp ${VIRTUAL_ENV_PATH}/bin/python
