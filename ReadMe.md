4a852160d9228e698b462ef767c1a99485559818
Pär Persson Mattsson <p.perssonmattsson@gmail.com>
2019-04-24 20:05:45 +0200

    Adds description of electron-rebuild.sh and corrects electron-rebuild.sh

--- ReadMe.md
+++ ReadMe.md
@@ -2,3 +2,7 @@
 
 A starting point for setting up an app with an Electron GUI and a Python backend.
 
+# electron-rebuild.sh
+Before every _npm install_ the command _electron-rebuild_ (node_modules/.bin/electron-rebuild) needs to be executed. However, for Node.js to be able to compile zeromq it needs a functioning installation of Python 2. This causes trouble if the python backend is written in Python3, and has a Python3 virtual environment.
+
+This script temporarily replaces the _python_ command with a symlink to Python 2, allowing Node.js to compile zeromq.
--- electron-rebuild.sh
+++ electron-rebuild.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
-PYTHON2_EXEC_PATH = "/usr/bin/python2.7"
+# Edit these values as necessary
 VIRTUAL_ENV_PATH="./venv"
+NODE_MODULES_PATH = "./node_modules"
+PYTHON2_EXEC_PATH = "/usr/bin/python2.7"
 
 # Move the existing python symlink to be able to create a new link to Python2.7
 mv ${VIRTUAL_ENV_PATH}/bin/python ${VIRTUAL_ENV_PATH}/bin/python_temp
@@ -9,7 +11,7 @@ mv ${VIRTUAL_ENV_PATH}/bin/python ${VIRTUAL_ENV_PATH}/bin/python_temp
 ln -s ${PYTHON2_EXEC_PATH} ${VIRTUAL_ENV_PATH}/bin/python
 
 # Rebuild electron
-./node_modules/.bin/electron-rebuild
+${NODE_MODULES_PATH}/.bin/electron-rebuild
 
 # Install/Compile packages
 npm install
