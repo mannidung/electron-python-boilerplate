fbbad33e01052eb650ca1facfecae3627af615f5
Pär Persson Mattsson <p.perssonmattsson@gmail.com>
2019-04-27 10:14:25 +0200

    Adds script for packaging application. At the moment it doesn't work.

---
+++ package-mac.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+# Run pyinstaller to package the server
+cd ./server
+# Create a one file bundle (with -F)
+pyinstaller -F server.py --distpath ../python_build
+
+cd ..
+./node_modules/.bin/electron-packager . --overwrite --platform=darwin --arch=x64 --prune=true --out=release-builds --ignore=^/server --ignore=^/npm_install\\.sh --ignore=^/package-mac\\.sh --ignore=^/README\\.md --ignore=^/venv_python2.7 --ignore=^/venv_python3
+
+# --icon=assets/icons/mac/icon.icns
